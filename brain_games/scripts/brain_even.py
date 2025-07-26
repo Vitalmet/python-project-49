e5c7b1f61b7ea683483ba6f09daf53d6f6f7621a
Vitalmet <vuyko87@bk.ru>
2025-07-26 10:08:51 +0300

    Made 'brain_even.py'

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,33 @@
+import random
+
+
+def is_even(number):
+    return number % 2 == 0
+
+
+def run_game():
+    print("Welcome to the Brain Even Game!")
+    name = input("May I have your name? ")
+    print(f"Hello, {name}!")
+
+    for _ in range(3):
+        number = random.randint(1, 100)
+        print(f"Question: {number}")
+        answer = input("Your answer (yes or no): ").strip().lower()
+
+        correct_answer = "yes" if is_even(number) else "no"
+
+        if answer == correct_answer:
+            print("Correct!")
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
+
+    print(f"Congratulations, {name}!")
+
+def main():
+    run_game()
+
+if __name__ == '__main__':
+    main()
