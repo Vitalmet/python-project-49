7e259b5da84aeb2271056a416e1f1ae96a443935
Vitalmet <vuyko87@bk.ru>
2025-07-26 13:09:47 +0300

    Made 'brain_gcd.py'

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,35 @@
+import random
+
+def greatest_common_divisor(num1, num2):
+    while num2 != 0:
+        remainder = num1 % num2  # Сохраняем остаток
+        num1 = num2              # Обновляем num1
+        num2 = remainder         # Обновляем num2
+    return num1
+
+def run_game():
+    print("Welcome to the Brain GCD Game!")
+    name = input("May I have your name? ")
+    print(f"Hello, {name}!")
+
+    for _ in range(3):
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 100)
+        print(f"Question: {num1} {num2}")
+        correct_answer = greatest_common_divisor(num1, num2)
+        answer = int(input("Your answer: "))
+
+        if answer == correct_answer:
+            print("Correct!")
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
+
+    print(f"Congratulations, {name}!")
+
+def main():
+    run_game()
+
+if __name__ == '__main__':
+    main()
